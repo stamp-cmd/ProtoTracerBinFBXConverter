c1e3976189b706925695ae2df18f6733c795b4f7
Stamp <76716475+stamp-cmd@users.noreply.github.com>
2024-12-11 13:34:45 +0700

    Update Converter.py

--- Converter.py
+++ Converter.py
@@ -3,26 +3,32 @@
 # MUST BE TRIANGULATED MODEL
 # Assume 100% weight on each shape key
 # input in meters, output in mm
-from FBXReader import *
+#=================================== (Remove *# to enable default ASCII FBX parser)
+#* from FBXReader import *
 from MorphCreator import *
-import os
+from B_FBXReader import *
+#* import os
 
 name = "UnicornZhenja"
 origFileName = f"Commissions\{name}.fbx"
-newFileName = f"Output\{name}Ascii.fbx"
 outputName = f"..\ProtoTracer\lib\ProtoTracer\Assets\Models\FBX\Commissions\{name}.h"
+#* newFileName = f"Output\{name}Ascii.fbx"
 
-os.system("FbXFormatConverter.exe -c \"" + origFileName + "\" -o \"" + newFileName + "\" -ascii")
+#* os.system("FbXFormatConverter.exe -c \"" + origFileName + "\" -o \"" + newFileName + "\" -ascii")
 
-with open(newFileName, 'r') as file:
-    data = file.read()
+#* with open(newFileName, 'r') as file:
+#*    data = file.read()
 
-    morphCreator = MorphCreator(GetMorphObject(data, name, 10.0)) #default is centimeters, prototracer uses millimeters
-
-    morphCodeString = morphCreator.GenerateMorphCode(name).expandtabs(4)
+#*    morphCreator = MorphCreator(GetMorphObject(data, name, 10.0)) #default is centimeters, prototracer uses millimeters
+morphCreator = MorphCreator(GetMorphObject(origFileName, name, 10.0)) #default is centimeters, prototracer uses millimeters
+#*    morphCodeString = morphCreator.GenerateMorphCode(name).expandtabs(4)
+morphCodeString = morphCreator.GenerateMorphCode(name).expandtabs(4)
 
     #print(morphCodeString)
-    f = open(outputName, "w")
-    f.write(morphCodeString)
-    f.close()
+#*    f = open(outputName, "w")
+#*    f.write(morphCodeString)
+#*    f.close()
+f = open(outputName, "w")
+f.write(morphCodeString)
+f.close()
     
