11021f6b17b146dd9fab0211993737088bb19559
Stamp <76716475+stamp-cmd@users.noreply.github.com>
2025-02-21 12:31:21 +0700

    Add seperated mesh and non-triangulated face detection

--- B_FBXReader.py
+++ B_FBXReader.py
@@ -45,6 +45,7 @@ class Node:
 # THE PIPELINE
 def GetMorphObject(filename: str, name: str, scale: float):
     NODE_UINT_BYTE = ("I", 4)
+    IGNORE_ERROR = False
     morph = MorphObject()
     with open(filename, "rb") as file:
         def read_node() -> Node:
@@ -69,7 +70,7 @@ def GetMorphObject(filename: str, name: str, scale: float):
                     current_node = read_node()
             return current_node
 
-        def get_content(byte: int):
+        def get_array(byte: int):
             array_len, encoding, compressed_len = struct.unpack("<3I", file.read(12))
             content = b""
             if encoding == 1:
@@ -81,15 +82,15 @@ def GetMorphObject(filename: str, name: str, scale: float):
         def read_prop():
             type_code = file.read(1).decode("ascii")
             if type_code == "L":
-                return struct.unpack("<Q", file.read(8))[0]
+                return struct.unpack("<q", file.read(8))[0]
             elif type_code == "S":
                 str_len = struct.unpack("<I", file.read(4))[0]
                 return file.read(str_len)
             elif type_code == "d":
-                array = get_content(8)
+                array = get_array(8)
                 return list(struct.unpack(f"<{array[0]}d", array[1]))
             elif type_code == "i":
-                array = get_content(4)
+                array = get_array(4)
                 return list(struct.unpack(f"<{array[0]}i", array[1]))
             else:
                 raise NotImplementedError(f"!!!TYPE CODE NOT IMPLEMENTED.TYPE CODE: \"{type_code}\"!!!")
@@ -110,66 +111,98 @@ def GetMorphObject(filename: str, name: str, scale: float):
         version = struct.unpack("<I", file.read(4))[0]
         if version > 7400:
             NODE_UINT_BYTE = ("Q", 8)
-            print("===USING UINT64 [VERSION >7.4]===")
         elif version < 7000:
-            raise NotImplementedError(f"!!!FBX VERSION {version} IS UNSUPPORTED, UPGRADE TO VERSION 7.4!!!")
+            raise NotImplementedError(f"!!!FBX VERSION {version} IS UNSUPPORTED, UPGRADE TO VERSION >7!!!")
+
+        models_id = []
+        models_name = []
+        rooted = False
 
         shape_keys = []
         base_mesh = Object3D()
         find_node(b"Objects")
-        geometry_node = find_node(b"Geometry")
-        while geometry_node:
+        object_node = read_node()
+        while not object_node.is_null():
             infos = []
-            for _ in range(geometry_node.num_properties):
-                infos.append(read_prop())
-            if b"Mesh" in infos:
-                props = get_nodes_prop([b"Vertices", b"PolygonVertexIndex"])
-                vertices = []
-                triangles = []
+            if object_node.name == b"Geometry":
+                for _ in range(object_node.num_properties):
+                    infos.append(read_prop())
+                if infos[2] == b"Mesh":
+                    props = get_nodes_prop([b"Vertices", b"PolygonVertexIndex"])
+                    vertices = []
+                    triangles = []
+                    for i in range(0, len(props[0]), 3):
+                        vertex = Vector3D()
+                        vertex.X, vertex.Y, vertex.Z = (
+                            props[0][i] * scale, props[0][i+1] * scale, props[0][i+2] * scale
+                        )
+                        print(f"Vertex: [{vertex.X}, {vertex.Y}, {vertex.Z}]")
+                        vertices.append(vertex)
+                    for i in range(0, len(props[1]), 3):
+                        triangle = Triangle()
+                        try:
+                            triangle.A, triangle.B, triangle.C = (
+                                props[1][i], props[1][i+1], (props[1][i+2] * -1) - 1
+                            )
+                        except IndexError as e:
+                            raise ValueError(f"!!!MESH NOT TRIANGULATED. MESH: {infos[1][:-10].decode('utf-8')}!!!") from e
+                        if (not (triangle.A > -1 and triangle.B > -1 and triangle.C > -1)) and (not IGNORE_ERROR):
+                            raise ValueError(f"!!!MESH NOT TRIANGULATED. MESH: {infos[1][:-10].decode('utf-8')}!!!")
+                        print(f"Triangle Vertex Indexes: [{triangle.A}, {triangle.B}, {triangle.C}]")
+                        triangles.append(triangle)
 
-                for i in range(0, len(props[0]), 3):
-                    vertex = Vector3D()
-                    vertex.X, vertex.Y, vertex.Z = (
-                        props[0][i] * scale, props[0][i+1] * scale, props[0][i+2] * scale
-                    )
-                    print(f"Vertex: [{vertex.X}, {vertex.Y}, {vertex.Z}]")
-                    vertices.append(vertex)
-                for i in range(0, len(props[1]), 3):
-                    triangle = Triangle()
-                    triangle.A, triangle.B, triangle.C = (
-                        props[1][i], props[1][i+1], (props[1][i+2] * -1) - 1
-                    )
-                    print(f"Triangle Vertex Indexes: [{triangle.A}, {triangle.B}, {triangle.C}]")
-                    triangles.append(triangle)
+                    base_mesh.TriangleCount, base_mesh.Triangles = (len(triangles), triangles)
+                    base_mesh.VertexCount, base_mesh.Vertices = (len(props[0]), vertices)
+                    print(f"Primary mesh contains {base_mesh.VertexCount} vertices and {base_mesh.TriangleCount} triangles.\nCreated primary mesh.")
 
-                base_mesh.TriangleCount, base_mesh.Triangles = (len(triangles), triangles)
-                base_mesh.VertexCount, base_mesh.Vertices = (len(props[0]), vertices)
-                print(f"Primary mesh contains {base_mesh.VertexCount} vertices and {base_mesh.TriangleCount} triangles.\nCreated primary mesh.")
-            elif b"Shape" in infos:
-                shape_key = ShapeKey()
-                shape_key.Name = list(
-                    filter(lambda i: isinstance(i, bytes) and b"\x00\x01Geometry" in i, infos)
-                    )[0][:-10].decode("utf-8")
-                props = get_nodes_prop([b"Indexes", b"Vertices"])
-                vertices = []
+                elif infos[2] == b"Shape":
+                    shape_key = ShapeKey()
+                    shape_key.Name = list(
+                        filter(lambda i: isinstance(i, bytes) and b"\x00\x01Geometry" in i, infos)
+                        )[0][:-10].decode("utf-8")
+                    props = get_nodes_prop([b"Indexes", b"Vertices"])
+                    vertices = []
 
-                for i in range(0, len(props[1]), 3):
-                    vertex = Vector3D()
-                    vertex.X, vertex.Y, vertex.Z = (
-                        props[1][i] * scale, props[1][i+1] * scale, props[1][i+2] * scale
-                    )
-                    vertices.append(vertex)
-                    print(f"Vertex: [{vertex.X}, {vertex.Y}, {vertex.Z}]")
+                    for i in range(0, len(props[1]), 3):
+                        vertex = Vector3D()
+                        vertex.X, vertex.Y, vertex.Z = (
+                            props[1][i] * scale, props[1][i+1] * scale, props[1][i+2] * scale
+                        )
+                        vertices.append(vertex)
+                        print(f"Vertex: [{vertex.X}, {vertex.Y}, {vertex.Z}]")
 
-                shape_key.IndexCount = len(props[0])
-                shape_key.Indexes = props[0]
-                shape_key.VertexCount = len(props[1])
-                shape_key.Vertices = vertices
-                shape_keys.append(shape_key)
-                print(f"Created shape key \"{shape_key.Name}\" with {shape_key.IndexCount} modified" +
-                f"indices and {shape_key.VertexCount} modified vertex values.")
-            file.seek(geometry_node.end_offset)
-            geometry_node = find_node(b"Geometry")
+                    shape_key.IndexCount = len(props[0])
+                    shape_key.Indexes = props[0]
+                    shape_key.VertexCount = len(props[1])
+                    shape_key.Vertices = vertices
+                    shape_keys.append(shape_key)
+                    print(f"Created shape key \"{shape_key.Name}\" with {shape_key.IndexCount} modified" +
+                    f"indices and {shape_key.VertexCount} modified vertex values.")
+
+            elif object_node.name == b"Model":
+                model_props = []
+                for i in range(object_node.num_properties):
+                    model_props.append(read_prop())
+                if model_props[2] == b"Mesh":
+                    models_id.append(model_props[0])
+                    models_name.append(model_props[1])
+            file.seek(object_node.end_offset)
+            object_node = read_node()
+
+        find_node(b"Connections")
+        conn_node = read_node()
+        while not conn_node.is_null():
+            conn_props = []
+            for i in range(conn_node.num_properties):
+                conn_props.append(read_prop())
+            #? See if mesh is connected to root
+            if (conn_props[1] in models_id) and (conn_props[2] == 0):
+                if not rooted:
+                    rooted = True
+                elif not IGNORE_ERROR:
+                    raise ValueError("!!!MULTIPLE MESH DETECTED. MESH: \""
+                    + models_name[models_id.index(conn_props[1])][:-7].decode("utf-8") + "\"!!!")
+            conn_node = read_node()
         print(f"Created {len(shape_keys)} shape keys.")
         morph.name = name
         morph.baseMesh = base_mesh
