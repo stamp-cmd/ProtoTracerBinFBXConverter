f75e4ddfc9d70690eaad39cda4de6d2dc60daa9c
Stamp <76716475+stamp-cmd@users.noreply.github.com>
2025-02-21 12:46:14 +0700

    Added FileNotFound failsafe

--- Converter.py
+++ Converter.py
@@ -4,17 +4,22 @@
 # Assume 100% weight on each shape key
 # input in meters, output in mm
 #=================================== (Remove *# to enable default ASCII FBX parser)
+from pathlib import Path
 from MorphCreator import *
 from B_FBXReader import *
 
 name = "NukudeFaceBinary"
-origFileName = f"Example/{name}.fbx"
-outputName = f"../ProtoTracer/lib/ProtoTracer/Assets/Models/FBX/Commissions/{name}.h"
+origFileName = Path(f"Example/{name}.fbx")
+outputName = Path(f"../ProtoTracer/lib/ProtoTracer/Assets/Models/FBX/Commissions/{name}.h")
 
 morphCreator = MorphCreator(GetMorphObject(origFileName, name, 10.0)) #default is centimeters, prototracer uses millimeters
 
 morphCodeString = morphCreator.GenerateMorphCode(name).expandtabs(4)
 
-f = open(outputName, "w")
+try:
+    f = open(outputName, "w")
+except FileNotFoundError as e:
+    print(f"###FILE NOT FOUND: \"{outputName}\". OUTPUT WILL BE PLACED HERE###")
+    f = open(f"{name}.h", "w")
 f.write(morphCodeString)
 f.close()
